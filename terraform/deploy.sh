b9f40cd959ecd9c9d9c7fb828986034376febd27
Lê Thành Long <48479168+KyohagiErika@users.noreply.github.com>
2022-10-19 10:15:50 +0700

    Update Terraform

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,7 +1,7 @@
-cd deploy-logs
-echo null >> /deploy-logs/terraform-test.log
-cd /terraform-test > /deploy-logs/terraform-test.log
-git pull > /deploy-logs/terraform-test.log
-docker compose down > /deploy-logs/terraform-test.log
-docker compose build --no-cache > /deploy-logs/terraform-test.log
-docker compose up -d > /deploy-logs/terraform-test.log
+cd ~/deploy-logs
+echo null >> ~/deploy-logs/terraform-test.log
+cd /terraform-test > ~/deploy-logs/terraform-test.log
+git pull > ~/deploy-logs/terraform-test.log
+docker compose down > ~/deploy-logs/terraform-test.log
+docker compose build --no-cache > ~/deploy-logs/terraform-test.log
+docker compose up -d > ~/deploy-logs/terraform-test.log
